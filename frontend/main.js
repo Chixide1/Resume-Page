0e5df651be9784a768741537d850f7e7288c2307
Chik <chixide12@gmail.com>
2024-01-19 23:22:40 +0000

    connected the frontend call to the backend

--- frontend/main.js
+++ frontend/main.js
@@ -1,22 +1,20 @@
-alert(screen.width);
+window.addEventListener('DOMContentLoaded', (event) =>{
+    getVisitCount();
+})
 
-// window.addEventListener('DOMContentLoaded', (event) =>{
-//     getVisitCount();
-// })
+const productionApiUrl = 'https://chikfuncs.azurewebsites.net/api/azureResumeCounter?code=lQxSzOthjy7qLL_lJ7kwbBXnLGgQcqkTJB4FQVVsx-UJAzFu2neVaQ==';
+const localapi = "http://localhost:7071/api/azureResumeCounter"
 
-// const productionApiUrl = 'https://azureresumevisitorcounter.azurewebsites.net/api/GetVisitorCounter?';
-// const localApiUrl = 'http://localhost:7071/api/GetVisitorCounter';
-
-// const getVisitCount = () => {
-//     let count = 30;
-//     fetch(productionApiUrl).then(response => {
-//         return response.json()
-//     }).then(response =>{
-//         console.log("Website called function API.");
-//         count =  response.count;
-//         document.getElementById("counter").innerText = count;
-//     }).catch(function(error){
-//         console.log(error);
-//     });
-//     return count;
-// }
+const getVisitCount = () => {
+    fetch(productionApiUrl).then(response => {
+        return response.json()
+    }).then(response =>{
+        console.log("Website called function API.");
+        count =  response;
+        
+        document.getElementById("counter").innerText = count;
+    }).catch(function(error){
+        console.log(error);
+    });
+    return count;
+}
