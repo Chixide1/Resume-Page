37abcc7aa0af6e170ef81cccd5f1213154e3576d
Chik <chixide12@gmail.com>
2024-01-22 14:25:24 +0000

    test

--- backend/tests/TEST-visitor_counter.py
+++ backend/tests/TEST-visitor_counter.py
@@ -1,7 +1,7 @@
 import unittest
 import sys
-# sys.path.append(r".\Azure\Azure-Resume\backend\api\azureResumeCounter")
-# sys.path.append(r"./backend/api/azureResumeCounter") #added python path to grab the test function
+sys.path.append(r"C:\Users\chixi\OneDrive\Desktop\Programming\Azure\Azure-Resume\backend\api\azureResumeCounter")
+sys.path.append(r"$HOME/work/Azure-Resume/Azure-Resume/backend/api/azureResumeCounter") #added python path to grab the test function
 from counter import updatecount
 
 class TestCounter(unittest.TestCase):
