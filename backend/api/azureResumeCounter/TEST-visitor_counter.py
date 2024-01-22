a23faab689f795547c85621418b5c3b71fe10856
Chik <chixide12@gmail.com>
2024-01-22 02:03:01 +0000

    added backend workflow

---
+++ backend/api/azureResumeCounter/TEST-visitor_counter.py
@@ -0,0 +1,11 @@
+import unittest
+from counter import updatecount
+ 
+class TestCounter(unittest.TestCase):
+
+    def test_updatecount(self):
+        result = updatecount({'PartitionKey': '0', 'RowKey': '0', 'count': "0"})
+        self.assertEqual(result, {'PartitionKey': '0', 'RowKey': '0', 'count': "1"})
+## Run the test
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
--- backend/api/azureResumeCounter/counter.py
+++ backend/api/azureResumeCounter/counter.py
@@ -16,11 +16,15 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         database = client.get_database_client(database_name) # Gets database we want to query from
         container = database.get_container_client(container_name) # Gets container inside the database we want to work with
         item = container.read_item("1", partition_key="1") # Finds item ID 1 in container
-        GetCountValue = item["count"] # Gets the value for count
-        IncrementValue = int(GetCountValue) + 1 # Increments count value by 1
-        item["count"] = str(IncrementValue) # Passes incremented count value back to dictionary
+        updated_item = updatecount(item) #increments count value
         updated_item = container.upsert_item(item) # Uploads dictionary back to the Cosmos DB with updated count value
     
         return func.HttpResponse(json.dumps(updated_item["count"]))
     except:
         return func.HttpResponse(status_code=400)
+    
+def updatecount(item):
+    count = item["count"] # Gets the value for count
+    count = int(count) + 1 # Increments count value by 1
+    item["count"] = str(count) # Passes incremented count value back to dictionary
+    return item
