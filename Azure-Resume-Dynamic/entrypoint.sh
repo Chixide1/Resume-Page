0894cf4735ade31d097ea25ca32a6aaf94ca5583
Chik <chixide12@gmail.com>
2024-03-14 23:54:31 +0000

    dev

--- Azure-Resume-Dynamic/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-python manage.py collectstatic --no-input
-gunicorn website.wsgi:application --bind 0.0.0.0:8000
--- Azure-Resume-Dynamic/project/portfolio_app/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Project, Certification, Work
-# Register your models here.
-
-admin.site.register(Project)
-admin.site.register(Certification)
-admin.site.register(Work)
