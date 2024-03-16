b778cf667a37fb07da3f517a3e6a0d4bcaa5f2e6
Chik <chixide12@gmail.com>
2024-03-16 03:03:20 +0000

    added my website into trusted origins

--- Azure-Resume-Dynamic/project/website/settings.py
+++ Azure-Resume-Dynamic/project/website/settings.py
@@ -128,4 +128,4 @@ STATIC_ROOT = BASE_DIR / '../static'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CSRF_TRUSTED_ORIGINS = ["https://localhost:8002", "https://project"]
+CSRF_TRUSTED_ORIGINS = ["https://localhost:8002", "https://project", "https://chikdoestech.xyz"]
