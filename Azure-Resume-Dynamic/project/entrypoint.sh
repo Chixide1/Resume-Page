b6c7a7dee2541cf28cfabf0090ce90c545634731
Chik <chixide12@gmail.com>
2024-03-16 17:43:48 +0000

    Removed Dynamic website as it didn't use azure

--- Azure-Resume-Dynamic/project/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-python manage.py migrate;
-python manage.py collectstatic --noinput;
-python manage.py createsuperuser --username $SUPER_USER_NAME --email $SUPER_USER_EMAIL --noinput;
-gunicorn website.wsgi:application --bind 0.0.0.0:8000;
