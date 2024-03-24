f4d97b8165654bb994b1d78290a7233f5e88af85
Chik <chixide12@gmail.com>
2024-03-24 14:13:39 +0000

    changed the function to anonymous

--- frontend/main.js
+++ frontend/main.js
@@ -2,7 +2,7 @@ window.addEventListener('DOMContentLoaded', (event) =>{
     getVisitCount();
 })
 
-const productionApiUrl = 'https://chikfuncs.azurewebsites.net/api/counter?code=MaEUxidZ0gIzFgqGhjcrFYRiFciziNF6EqbiVTMD9BZaAzFuK24sCw==';
+const productionApiUrl = 'https://chikfuncs.azurewebsites.net/api/counter';
 const localapi = "http://localhost:7071/api/counter"
 
 async function getVisitCount() {
