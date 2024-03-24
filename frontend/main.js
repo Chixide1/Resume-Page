074f0a27d455b5e4d0b3bac78714195d59d924f2
Chik <chixide12@gmail.com>
2024-03-24 02:29:38 +0000

    testing

--- frontend/main.js
+++ frontend/main.js
@@ -5,16 +5,32 @@ window.addEventListener('DOMContentLoaded', (event) =>{
 const productionApiUrl = 'https://chikfuncs.azurewebsites.net/api/counter?code=MaEUxidZ0gIzFgqGhjcrFYRiFciziNF6EqbiVTMD9BZaAzFuK24sCw==';
 const localapi = "http://localhost:7071/api/counter"
 
-const getVisitCount = () => {
-    fetch(productionApiUrl).then(response => {
-        return response.json()
-    }).then(response =>{
-        console.log("Website called function API.");
-        count =  response;
-        
-        document.getElementById("counter").innerText = count;
-    }).catch(function(error){
-        console.log(error);
-    });
-    return count;
+async function getVisitCount() {
+
+  try {
+
+    const response = await fetch(productionApiUrl);
+
+    const data = await response.json();
+
+  
+
+    console.log("Website called function API.");
+
+    count = data;
+
+    document.getElementById("counter").innerText = count;
+
+  
+
+    return count; // Return the count after receiving the response
+
+  } catch (error) {
+
+    console.error("Error fetching API response:", error);
+
+    throw error; // Re-throw the error for further handling if needed
+
+  }
+
 }
