211bc48bf52d0774af2ab920c99b2a2d746ddee5
Chik <chixide12@gmail.com>
2024-03-24 01:24:13 +0000

    amended workflows

--- frontend/main.js
+++ frontend/main.js
@@ -2,8 +2,8 @@ window.addEventListener('DOMContentLoaded', (event) =>{
     getVisitCount();
 })
 
-const productionApiUrl = 'https://chikfuncs.azurewebsites.net/api/azureResumeCounter?code=lQxSzOthjy7qLL_lJ7kwbBXnLGgQcqkTJB4FQVVsx-UJAzFu2neVbQ==';
-const localapi = "http://localhost:7071/api/azureResumeCounter"
+const productionApiUrl = 'https://chikfuncs.azurewebsites.net/api/counter?code=MaEUxidZ0gIzFgqGhjcrFYRiFciziNF6EqbiVTMD9BZaAzFuK24sCw==';
+const localapi = "http://localhost:7071/api/counter"
 
 const getVisitCount = () => {
     fetch(productionApiUrl).then(response => {
